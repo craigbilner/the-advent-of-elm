270053a9a89cfc348955bcf15e0fcb36735c35a5
Craig Bilner <craig.bilner@gmail.com>
2016-03-06 20:39:52 +0000

    Finish day 9

--- src/ElvesLookElvesSay/ElvesLookElvesSay.elm
+++ src/ElvesLookElvesSay/ElvesLookElvesSay.elm
@@ -1,5 +1,36 @@
 module ElvesLookElvesSay where
 
+import String
+
+
+import Utils
+
+splitAtChange : String -> List (String, Int) -> List (String, Int)
+splitAtChange char groups =
+        case groups of
+                [] ->
+                        (char, 1)::groups
+
+                h::t ->
+                        if (fst h) == char
+                           then (char, (snd h) + 1)::t
+                           else (char, 1)::h::t                        
+
+groupsToString :(String, Int) -> String -> String
+groupsToString group newString =
+        newString ++ (toString (snd group)) ++ (fst group)
+
+groupByNumber : String -> String
+groupByNumber =
+        String.split ""
+        >> List.foldr splitAtChange []
+        >> List.foldl groupsToString ""
+
+runNumbers : Int -> String -> String
+runNumbers _ num =
+        groupByNumber num
+
 run : Int -> Int -> Int
 run iterations input =
-        0
+        List.foldl runNumbers (toString input) [1..iterations]
+        |> String.length
